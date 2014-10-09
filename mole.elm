083fef9b4934cead741486c7737bf618cf101e8f
Michael James <michael@michaelbjames.com>
2014-10-09 14:17:42 -0400

    added images, no random so far

--- mole.elm
+++ mole.elm
@@ -1,20 +1,20 @@
 import Window
 import Mouse
-import Random
 import Graphics.Input
+import Debug
 
 data Input
     = Window (Int, Int)
-    | ToD Time -- Update every 300ms
-    | Random Float
+    | GameTick -- Update every 300ms
     | Whack
 
 type Status = {
     score : Int,
-    ttl : Time,
     playsLeft : Int,
     moleOnScreen : Bool,
-    windowSize : (Int, Int)
+    windowSize : (Int, Int),
+    targetPosition : (Int, Int),
+    doMole : Bool
     }
 
 targetInput : Graphics.Input.Input ()
@@ -24,8 +24,54 @@ inputs : Signal Input
 inputs = merges [
     lift Window Window.dimensions,
     lift (always Whack) <| targetInput.signal,
-    lift ToD <| every 300,
-    lift Random <| Random.float <| every 300
+    lift (always GameTick) <| every second
     ]
 
-main = lift asText inputs
+emptyStatus = {
+    score = 0,
+    playsLeft = 10,
+    moleOnScreen = False,
+    windowSize = (500, 500),
+    targetPosition = (250, 250),
+    doMole = True
+    }
+
+update : Input -> Status -> Status
+update input status = case input of
+    Window xy -> {status | windowSize <- xy}
+    GameTick -> {status |
+        moleOnScreen <- not status.moleOnScreen,
+        playsLeft <- if status.playsLeft > 0 then status.playsLeft - 1 else 0
+        }
+    Whack -> {status |
+        score <- if status.doMole then status.score + 1 else status.score - 10,
+        moleOnScreen <- False
+        }
+
+gameState : Signal Status
+gameState = foldp update emptyStatus inputs
+
+main = lift draw (lift (Debug.watch "Game") gameState)
+
+draw : Status -> Element
+draw status =
+    if  | status.moleOnScreen && status.playsLeft > 0 -> targetElement status
+        | status.playsLeft == 0 -> asText status.score
+        | otherwise -> empty
+
+
+targetElement : Status -> Element
+targetElement status =
+    let moleImg = image 50 50 "img/mole.png"
+            |> Graphics.Input.clickable targetInput.handle ()
+        elephantImg = image 228 158 "img/elephant.png"
+            |> Graphics.Input.clickable targetInput.handle ()
+        img = if status.doMole then moleImg else elephantImg
+            -- BUG! I should be able to apply clickable to this if statement
+        windowX = status.windowSize |> fst
+        windowY = status.windowSize |> snd
+        xPosition = status.targetPosition |> fst |> absolute
+        yPosition = status.targetPosition |> snd |> absolute
+        position = middleAt xPosition yPosition
+    in
+        container windowX windowY position img
